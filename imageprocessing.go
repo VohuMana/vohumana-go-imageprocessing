a2078a7219b20631affed6c4e7df4c982300458a
David James <icyflame198@yahoo.com>
2016-03-02 23:39:52 -0800

    Added a new way to choose filter from command line

--- imageprocessing.go
+++ imageprocessing.go
@@ -21,11 +21,13 @@ func main() {
 	var imageName string
 	var outputFormat string
 	var outputName string
+	var operation string
 	
 	// Get command line parameter for html file
 	flag.StringVar(&imageName, "img", "", "filename of the image to be loaded and modified")
 	flag.StringVar(&outputFormat, "fmt", "", "output format. Possible options are png or jpg")
 	flag.StringVar(&outputName, "out", "", "output filename without trailing .png or .jpg")
+	flag.StringVar(&operation, "op", "HistogramNormalization", "Image operation to run on input.  Possible values are HistogramNormalization, ExtractRedChannel, ExtractGreenChannel, ExtractBlueChannel")
 	flag.Parse()
 
 	if (imageName == "" || outputName == "" || (outputFormat != "png" && outputFormat != "jpg")) {
@@ -36,11 +38,12 @@ func main() {
 	file, err := os.Open(imageName)
 	checkError(err)
 
-	var histogramFilter filters.HistogramEqualizationFilter
+	filter := filters.ImageFilterMap[operation]
+
 	img, _, err := image.Decode(file)
 	checkError(err)
 
-	newImage := histogramFilter.Apply(img)
+	newImage := filter.Apply(img)
 
 	switch (outputFormat) {
 	case "png":
